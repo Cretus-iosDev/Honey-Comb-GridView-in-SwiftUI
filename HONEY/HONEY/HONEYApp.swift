fead9078aa9385f5d51cafb850750b3ea21599cc
Rutik Maraskolhe <84556881+Cretus-iosDev@users.noreply.github.com>
2021-07-02 09:18:51 +0530

    Add files via upload

---
+++ HONEY/HONEY/HONEYApp.swift
@@ -0,0 +1,17 @@
+//
+//  HONEYApp.swift
+//  HONEY
+//
+//  Created by rutik maraskolhe on 01/07/21.
+//
+
+import SwiftUI
+
+@main
+struct HONEYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
